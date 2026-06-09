7b8c9cd926aae7969c025c80a6e655ae6a08f55d
chesarandatia@gmail.comgit status$ git config --global user.email chesarandatia@gmail.comv <chesarandatia@gmail.com>
2026-06-09 17:07:06 +0700

    void fungsi dan showHistory

--- Project_Uap_SDA.cpp
+++ Project_Uap_SDA.cpp
@@ -5,32 +5,46 @@
 
 using namespace std;
 
-
-
-int main() {
-    
-            int seats;
-            cout << "Masukkan jumlah kursi: ";
-            cin >> seats;
-            if (seats <= 0) {
-                cout << "Jumlah kursi harus lebih dari 0.\n";
-                continue;
-            }
-            Ticket* ticket = system.createBooking(film, seats);
-            cout << "Tiket berhasil dipesan: ID " << ticket->id << ", film " << ticket->film << ", kursi " << ticket->seats << ".\n";
-            delete ticket;
-        } else if (choice == 2) {
-            system.processNextBooking();
-        } else if (choice == 3) {
-            system.showQueue();
-        } else if (choice == 4) {
-            system.showHistory();
-        } else if (choice == 5) {
-            cout << "Keluar dari program.\n";
-        } else {
-            cout << "Pilihan tidak dikenal. Silakan coba lagi.\n";
+void showHistory() const {
+        if (history.empty()) {
+            cout << "Belum ada riwayat pemesanan.\n";
+            return;
+        }
+        cout << "Riwayat pemesanan:\n";
+        stack<Ticket> copy = history;
+        while (!copy.empty()) {
+            Ticket ticket = copy.top();
+            copy.pop();
+            cout << "ID " << ticket.id << " - " << ticket.film << " (" << ticket.seats << " kursi)\n";
         }
     }
+};
 
-    return 0;
-}
+int main() {
+    BookingSystem system;
+    int choice = 0;
+
+    while (choice != 5) {
+        cout << "\n=== Sistem Pemesanan Tiket Film Sederhana ===\n";
+        cout << "1. Pesan tiket\n";
+        cout << "2. Proses tiket berikutnya\n";
+        cout << "3. Lihat antrian tiket\n";
+        cout << "4. Lihat riwayat pemesanan\n";
+        cout << "5. Keluar\n";
+        cout << "Pilih menu: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            system.showFilms();
+            int filmChoice = 0;
+            cout << "Pilih nomor film: ";
+            cin >> filmChoice;
+            string film;
+            switch (filmChoice) {
+                case 1: film = "spiderman"; break;
+                case 2: film = "ironman"; break;
+                case 3: film = "mermaidman"; break;
+                default:
+                    cout << "Pilihan film tidak valid.\n";
+                    continue;
+            }
